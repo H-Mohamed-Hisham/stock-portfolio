2172fa1f60fa6b7956b7fc53c3f08e3e08863e6b
H-Mohamed-Hisham <hmhwebdev@gmail.com>
2024-09-27 23:42:48 +0530

    Minor UI Change

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -3,9 +3,12 @@ import { Link } from "react-router-dom";
 // Constants
 import { SIGN_IN_URL } from "@/constants/routes";
 
+// Shadcn
+import { Button } from "@/components/ui/button";
+
 export function Home() {
   return (
-    <div className="relative isolate px-6 pt-14 lg:px-8">
+    <div className="relative isolate">
       <div
         aria-hidden="true"
         className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
@@ -18,21 +21,18 @@ export function Home() {
           className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
         />
       </div>
-      <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+      <div className="mx-auto max-w-2xl py-32 sm:py-48">
         <div className="text-center">
           <h1 className="text-4xl font-bold tracking-tight text-foreground sm:text-6xl">
-            Stock portfolio tracker
+            Stock Portfolio Tracker
           </h1>
           <p className="mt-6 text-lg leading-8 text-primary">
             Easy way to manage and track your investments in stocks & index
           </p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
-            <Link
-              to={SIGN_IN_URL}
-              className="rounded-md bg-primary text-primary-foreground px-3.5 py-2.5 text-sm font-semibold shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary"
-            >
-              Get started
-            </Link>
+            <Button asChild className="font-semibold">
+              <Link to={SIGN_IN_URL}>Get started</Link>
+            </Button>
           </div>
         </div>
       </div>
